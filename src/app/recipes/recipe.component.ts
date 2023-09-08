c3c82546d9a06a5e953d75fbee8ed1847f138734
Miro29147 <miro29147@gmail.com>
2023-09-08 11:35:55 +0300

    Creating project

---
+++ src/app/recipes/recipe.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.css'],
+})
+export class RecipesComponent {}
