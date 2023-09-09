07ae9466e4d6908967025c41d1b1d6a6226e9fba
Miro29147 <miro29147@gmail.com>
2023-09-09 12:23:06 +0300

    dropdown directive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,6 +3,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styles: [
+    `
+      .show + .dropdown-menu {
+        display: block;
+        transform: translate(-70px, -7px);
+      }
+    `,
+  ],
 })
 export class HeaderComponent {
   @Output() featureSelected = new EventEmitter<string>();
---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.show') isOpen = false;
+
+  constructor() {}
+
+  @HostListener('click') toggleOpen() {
+    console.log(this.isOpen);
+    this.isOpen = !this.isOpen;
+  }
+}
