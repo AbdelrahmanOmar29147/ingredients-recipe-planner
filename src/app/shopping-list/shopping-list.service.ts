074cc7346007938b484b35aedc285c94b08d5a72
Miro29147 <miro29147@gmail.com>
2023-09-10 18:26:47 +0300

    Adding observables

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 export default class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  subscription = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -14,6 +14,6 @@ export default class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.subscription.next(this.ingredients.slice());
   }
 }
